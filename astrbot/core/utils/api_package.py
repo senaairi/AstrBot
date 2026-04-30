c5e06c6c4179ead77c388e2d031933976aec408d
SenaAiri <senaairi@users.noreply.github.com>
2026-04-30 16:42:00 +0800

    api package工具优化

--- astrbot/core/utils/api_package.py
+++ astrbot/core/utils/api_package.py
@@ -13,6 +13,7 @@ class InvalidSignatureError(Exception):
 
 
 def de_package(apikey: str, data: str, noise: str, expiry_date: str, signature: str) -> dict:
+    """验证签名，解包请求参数"""
     if not data:
         raise InvalidSignatureError("data is empty")
     if not noise:
@@ -29,7 +30,6 @@ def de_package(apikey: str, data: str, noise: str, expiry_date: str, signature: 
         raise InvalidSignatureError("expiry_date is expired")
 
     payload = f"{data}{noise}{expiry_date}{apikey}"
-    #computed = hashlib.sha256(payload.encode("utf-8")).hexdigest()
     computed = hmac.new(apikey.encode("utf-8"), payload.encode("utf-8"), hashlib.sha256).hexdigest()
 
     if not hmac.compare_digest(computed, signature):
@@ -44,15 +44,24 @@ def de_package(apikey: str, data: str, noise: str, expiry_date: str, signature: 
 
     return result
 
-# 漏掉了重要的内容：astrbot后端不存储明文apikey，所以在使用本函数前，你需要将apykey先进行编码
-# 示例：hashlib.pbkdf2_hmac( "sha256", {raw_key}.encode("utf-8"), b"astrbot_api_key", 100_000, ).hex()
+
+def apikey_hash(apikey: str) -> str:
+    """获取原始apikey的hash值"""
+    return hashlib.pbkdf2_hmac(
+        "sha256",
+        apikey.encode("utf-8"),
+        b"astrbot_api_key",
+        100_000,
+    ).hex()
+
+
 def en_package(appid: str, apikey: str, data: dict) -> dict:
+    """apikey需要先用`apikey_hash`后才能传入使用"""
     encode_data = base64.b64encode(
         json.dumps(data, separators=(",", ":"), ensure_ascii=False).encode("utf-8")
     ).decode("utf-8")
     noise = secrets.token_urlsafe(32)
     expiry_date = (datetime.now().astimezone() + timedelta(days=1)).replace(microsecond=0).isoformat()
-    #signature = hashlib.sha256(f"{encode_data}{noise}{expiry_date}{apikey}".encode()).hexdigest()
     payload = f"{encode_data}{noise}{expiry_date}{apikey}"
     signature = hmac.new(apikey.encode("utf-8"), payload.encode("utf-8"), hashlib.sha256).hexdigest()
 
@@ -65,8 +74,10 @@ def en_package(appid: str, apikey: str, data: dict) -> dict:
     }
 
 
-# 按顺序获取输入参数：json -> form -> query -> header
+
 async def request_input(name: list) -> dict:
+    """按顺序获取输入参数：json -> form -> query -> header"""
+
     json_data = await request.get_json(silent=True) or {}
     form_data = (await request.form).to_dict() or {}
 
